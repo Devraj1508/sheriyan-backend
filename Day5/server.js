297293bf453a54c6317385240b3e6358c216884a
Devraj <your-email@example.com>
2026-07-08 01:28:41 +0530

    Day -5

---
+++ Day5/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+console.log(process.env.MONGO_URI);
+const app=require('./src/app');
+const connectTodb=require('./src/config/database')
+
+connectTodb(); 
+app.listen(3000,()=>{
+    console.log("server is running on the port 3000")
+})
