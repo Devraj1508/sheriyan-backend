05f0e9c0408ba7bf23d35e457e02a7fa467de7a5
Devraj <your-email@example.com>
2026-07-08 01:49:06 +0530

    Your commit message

---
+++ src/Models/notesmodels.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose")
+
+const notesschema= new mongoose.Schema({
+    title:String,
+    description:String
+})
+const notesmodel=mongoose.model("notes",notesschema)
+
+module.exports=notesmodel
